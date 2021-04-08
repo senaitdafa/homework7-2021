3d2399991afbda2c8d91f46b358e383d8cc72091
senaitdafa <sdafa@umich.edu>
2021-04-07 21:34:16 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -6,13 +6,13 @@ window.addEventListener("load", function() {
 
 });
 
- document.querySelector("#play").addEventListener("click", function() {
+document.querySelector("#play").addEventListener("click", function() {
  	console.log("Play Video");
 	video.play();
 	let vol = document.querySelector("#volume").innerHTML = video.volume;
  });
 
- document.querySelector("#pause").addEventListener("click", function() {
+document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
 	video.pause();
 });

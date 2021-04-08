d5a353c042144cbb9782c6084bd7ba1ddc00bd5e
senaitdafa <sdafa@umich.edu>
2021-04-07 21:49:05 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -3,16 +3,15 @@ var video;
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
 	video = document.querySelector("#video");
-
 });
 
 document.querySelector("#play").addEventListener("click", function() {
  	console.log("Play Video");
-	video.play();
-	let vol = document.querySelector("#volume").innerHTML = video.volume;
+	//video.play();
+	//let vol = document.querySelector("#volume").innerHTML = video.volume;
  });
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
-	video.pause();
+	//video.pause();
 });

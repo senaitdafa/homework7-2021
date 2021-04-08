eef28b95e3eda5a299d2b2e1c724e581903acb73
senaitdafa <sdafa@umich.edu>
2021-04-07 23:27:23 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
  	console.log("Play Video");
 	video.play();
-	let vol = document.querySelector("#volume").innerHTML = video.volume;
+	document.querySelector("#volume").innerHTML = video.volume;
  });
 
 document.querySelector("#pause").addEventListener("click", function() {
@@ -38,14 +38,23 @@ document.querySelector("#skip").addEventListener("click", function() {
 });
 
 document.querySelector("#mute").addEventListener("click", function(){
+
 	if (video.muted === false){
+		console.log("Muted Video")
 		video.muted = true;
-		document.querySelector("#mute").innerHTML = "Unmute";
+		this.innerHTML = "Unmute";
 	}
 	else{
+		console.log("Unmuted Video")
 		video.muted = false;
-		document.querySelector("#mute").innerHTML = "Mute";
+		this.innerHTML = "Mute";
 
 	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	console.log("update slider number");
+	document.querySelector("#volume").innerHTML = this.value;
+
 
 });
